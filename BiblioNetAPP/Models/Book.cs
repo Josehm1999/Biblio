8b47271f273bb941646864d67a95c83bf5b0bc70
JoseHM <josehm1999@gmail.com>
2022-03-17 10:05:40 -0500

    "BiblioNetAPP: First commit"

---
+++ BiblioNetAPP/Models/Book.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BiblioNetAPP.Models
+{
+
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        public string BookName { get; set; }
+
+        public int Author { get; set; }
+
+        public decimal Price { get; set; }
+
+    }
+}
